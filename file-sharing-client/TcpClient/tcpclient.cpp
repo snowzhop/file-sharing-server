9d300944efd9b85f6a540156a153ab15d47ba941
Поликарп <snowzhop@gmail.com>
2020-05-24 01:02:35 +0300

    TcpClient moved to own folder. Added some functions.

--- file-sharing-client/TcpClient/tcpclient.cpp
+++ file-sharing-client/TcpClient/tcpclient.cpp
@@ -1,7 +1,26 @@
 #include "tcpclient.h"
 
 TcpClient::TcpClient(QObject* parent) : QTcpSocket(parent) {
-
 }
 
+qint64 TcpClient::encryptAndSend(const u_char* data, size_t length) {
+    std::string encyptedData = cryptographer.encryptData(data, length);
+    return QTcpSocket::write(encyptedData.c_str(), encyptedData.length());
+}
 
+QByteArray TcpClient::decryptData(const u_char* data, size_t length) {
+    std::string decryptedData = cryptographer.decryptData(data, length);
+    return QByteArray(decryptedData.c_str(), decryptedData.length());
+}
+
+const unsigned char* TcpClient::getPackedPublicKey() {
+    return cryptographer.getPublicKey();
+}
+
+void TcpClient::calculateSecretKey(const unsigned char* key) {
+    cryptographer.deriveSecretKey(key);
+}
+
+const unsigned char* TcpClient::getSecretKey() {
+    return cryptographer.getSecretKey();
+}
--- file-sharing-client/TcpClient/tcpclient.h
+++ file-sharing-client/TcpClient/tcpclient.h
@@ -10,10 +10,17 @@ class TcpClient : public QTcpSocket {
 public:
     TcpClient(QObject* parent = nullptr);
 
+    const unsigned char* getPackedPublicKey();
+    void calculateSecretKey(const u_char* key);
+    qint64 encryptAndSend(const u_char* data, size_t length);
+    QByteArray decryptData(const u_char* data, size_t length);
+
+    // test function
+    const unsigned char* getSecretKey();
+
 private:
-    void clientHandshake();
 
-
+    EcCrypto cryptographer;
 };
 
 #endif // TCPCLIENT_H
