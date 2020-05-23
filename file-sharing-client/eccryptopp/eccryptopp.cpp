bdcb2f16584be03ec77e92601281c0bd1454a169
Поликарп <snowzhop@gmail.com>
2020-05-24 01:04:39 +0300

    Little fixes.

--- file-sharing-client/eccryptopp/eccryptopp.cpp
+++ file-sharing-client/eccryptopp/eccryptopp.cpp
@@ -10,7 +10,7 @@ using CryptoPP::Integer;
 // Default EcCrypto ctor
 EcCrypto::EcCrypto() {
     group.Initialize(CryptoPP::ASN1::secp256r1());
-    
+
     CryptoPP::AutoSeededX917RNG<CryptoPP::AES> rng;
     privateKey.Randomize(rng, CryptoPP::Integer::One(), group.GetMaxExponent());
 }
@@ -30,16 +30,16 @@ CryptoPP::byte* EcCrypto::packKey(const CryptoPP::Integer &x, const CryptoPP::In
     }
     byte* packedKey = new byte[66]; /* X-coord(32) + Y-coord(32) + 2 bytes(for type of key) */
     byte* buffer = new byte[32];
-    
+
     packedKey[0] = '0';
     packedKey[1] = '4';
-    
+
     x.Encode(buffer, 32);
     std::strncpy(reinterpret_cast<char*>(packedKey + 2), reinterpret_cast<char*>(buffer), 32);
-    
+
     y.Encode(buffer, 32);
     std::strncpy(reinterpret_cast<char*>(packedKey + 2 + 32), reinterpret_cast<char*>(buffer), 32);
-    
+
     delete[] buffer;
     return packedKey;
 }
@@ -48,48 +48,45 @@ CryptoPP::ECPPoint EcCrypto::unpackKey(const byte *key) {
     if (key[0] != '0' || key[1] != '4') {
         throw std::runtime_error("Wrong key form!");
     }
-    
+
     int offset = 32; /* Byte offset for each coordinate */
-    
+
     byte* rawX = new byte[32];
     byte* rawY = new byte[32];
-    
+
     for (int i = 2; i < offset + 2; ++i) { /* Skips two first bytes */
         rawX[i-2] = key[i];
     }
     for (int i = offset + 2; i < 2*offset + 2; ++i) { /* Skips two first bytes and X-coordinate*/
         rawY[i-offset-2] = key[i];
     }
-    
+
     return CryptoPP::ECPPoint(Integer(rawX, 32), Integer(rawY, 32));
 }
 
 byte* EcCrypto::getPublicKey() {
     Element publicKey = group.ExponentiateBase(privateKey);
-    
+
     return packKey(publicKey);
 }
 
 void EcCrypto::deriveSecretKey(const CryptoPP::ECPPoint& point) {
     secretKey = new byte[32];
-    
+
     Element shared = group.GetCurve().ScalarMultiply(point, privateKey);
-    
+
     byte* tmpBuffer = new byte[shared.x.ByteCount()];
     shared.x.Encode(tmpBuffer, shared.x.ByteCount());
-    
+
     CryptoPP::SHA256 sha256;
     sha256.CalculateDigest(secretKey, tmpBuffer, shared.x.ByteCount());
-    
+
     /* TODO replace iv calculation way */
     iv = new byte[IV_SIZE];
     for (int i = 0; i < IV_SIZE; ++i) {
         iv[i] = secretKey[i];
     }
-    
-    encryptor.SetKeyWithIV(secretKey, 32, iv, IV_SIZE);
-    decryptor.SetKeyWithIV(secretKey, 32, iv, IV_SIZE);
-    
+
     delete[] tmpBuffer;
 }
 
@@ -103,32 +100,36 @@ void EcCrypto::deriveSecretKey(const CryptoPP::byte* anotherKey) {
 
 std::string EcCrypto::encryptData(const CryptoPP::byte *data, size_t length) {
     std::string buffer;
-    
+
+    encryptor.SetKeyWithIV(secretKey, 32, iv, IV_SIZE);
+
     CryptoPP::StringSource(data, length, true,
-            new CryptoPP::AuthenticatedEncryptionFilter(encryptor,
-                    new CryptoPP::StringSink(buffer), false, TAG_SIZE));
-    
-    return std::move(buffer);
+                           new CryptoPP::AuthenticatedEncryptionFilter(encryptor,
+                                   new CryptoPP::StringSink(buffer), false, TAG_SIZE));
+
+    return buffer;
 }
 
-std::string EcCrypto::decryptData(CryptoPP::byte *data, size_t length) {
+std::string EcCrypto::decryptData(const CryptoPP::byte* data, size_t length) {
     std::string buffer;
-    
+
+    decryptor.SetKeyWithIV(secretKey, 32, iv, IV_SIZE);
+
     CryptoPP::AuthenticatedDecryptionFilter df(
-            decryptor,
-            new CryptoPP::StringSink(buffer),
-            CryptoPP::AuthenticatedDecryptionFilter::DEFAULT_FLAGS,
-            TAG_SIZE
-            );
-    
+        decryptor,
+        new CryptoPP::StringSink(buffer),
+        CryptoPP::AuthenticatedDecryptionFilter::DEFAULT_FLAGS,
+        TAG_SIZE
+    );
+
     CryptoPP::StringSource(data, length, true, new CryptoPP::Redirector(df));
-    
+
     if (!df.GetLastResult()) {
         // TODO add exception description
         throw std::runtime_error("");
     }
-    
-    return std::move(buffer);
+
+    return buffer;
 }
 
 const CryptoPP::byte* EcCrypto::getSecretKey() {
--- file-sharing-client/eccryptopp/eccryptopp.h
+++ file-sharing-client/eccryptopp/eccryptopp.h
@@ -11,23 +11,23 @@ class EcCrypto {
 private:
     using GroupParameters = CryptoPP::DL_GroupParameters_EC<CryptoPP::ECP>;
     using Element = CryptoPP::DL_GroupParameters_EC<CryptoPP::ECP>::Element; // equals ECPPoint
-    
+
     const int TAG_SIZE = 16;
     const int IV_SIZE = 12;
-    
+
     CryptoPP::GCM<CryptoPP::AES>::Encryption encryptor;
     CryptoPP::GCM<CryptoPP::AES>::Decryption decryptor;
     GroupParameters     group;
     CryptoPP::Integer   privateKey;
     CryptoPP::byte*     secretKey = nullptr;
     CryptoPP::byte*     iv = nullptr;
-    
+
 public:
-    
+
     /// \brief In default constructor program generates new keys
     EcCrypto();
     ~EcCrypto();
-    
+
     /// \brief Packs two CryptoPP::Integer points into one complete point
     /// \param point the elliptic curve (CryptoPP::ASN1::secp256r1) point
     /// \return elliptic curve point as complete point
@@ -41,11 +41,11 @@ public:
     /// \param key complete elliptic curve (CryptoPP::ASN1::secp256r1) point
     /// \return separate X and Y coordinates in one ECPPoint object
     static CryptoPP::ECPPoint unpackKey(const CryptoPP::byte* key);
-    
+
     /// \brief Returns complete public key
     /// \return public key as complete point
     CryptoPP::byte* getPublicKey();
-    
+
     /// \brief Creates secret key using others public key as ECPPoint
     /// \param point elliptic curve (CryptoPP::ASN1::secp256r1) point
     void deriveSecretKey(const CryptoPP::ECPPoint& point);
@@ -56,16 +56,16 @@ public:
     /// \brief Creates secret key using others public key as one complete point
     /// \param anotherKey others public key as complete key
     void deriveSecretKey(const CryptoPP::byte* anotherKey);
-    
+
     /// \brief Encrypts user's data using AES-256 GCM
     /// \param data user's data
     /// \return encrypted data
     std::string encryptData(const CryptoPP::byte* data, size_t length);
-    /// \brief Decrypts data which was encrypted by AES-256 GCM
+    /// \brief Decrypts data which was encrypted using AES-256 GCM
     /// \param data encrypted data
     /// \return decrypted data
-    std::string decryptData(CryptoPP::byte* data, size_t length);
-    
+    std::string decryptData(const CryptoPP::byte* data, size_t length);
+
     /* Temporary methods */
     const CryptoPP::byte* getSecretKey();
     /* Temporary methods */
