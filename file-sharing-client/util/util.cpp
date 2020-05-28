0c3999c827e88401c9699e373cb73efc46b74303
Поликарп <snowzhop@gmail.com>
2020-05-28 19:28:12 +0300

    Commas was deleted

--- file-sharing-client/util/util.cpp
+++ file-sharing-client/util/util.cpp
@@ -10,7 +10,7 @@ void Util::printBytes(const unsigned char* data, unsigned long size) {
 
     tmp.append(std::to_string(data[0]));
     for (size_t i = 1; i < size-1; ++i) {
-        tmp.append(", ");
+        tmp.append(" ");
         tmp.append(std::to_string(data[i]));
     }
     tmp.append(", ").append(std::to_string(data[size-1])).append("]");
@@ -19,7 +19,6 @@ void Util::printBytes(const unsigned char* data, unsigned long size) {
 }
 
 u_int Util::byteRepresentationToUint(const QByteArray& str) {
-    qDebug() << "str.length():" << str.length();
     return u_int(static_cast<u_char>(str[0]) << 24 |
                  static_cast<u_char>(str[1]) << 16 |
                  static_cast<u_char>(str[2]) << 8  |
