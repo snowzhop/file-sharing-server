42b608aeb8d7b1412e328cf473eceaa827e784e9
Поликарп <snowzhop@gmail.com>
2020-05-30 00:58:30 +0300

    Cryptographer replaced by pointer. Added method for key generation

--- file-sharing-client/TcpClient/tcpclient.cpp
+++ file-sharing-client/TcpClient/tcpclient.cpp
@@ -1,26 +1,48 @@
 #include "tcpclient.h"
 
-TcpClient::TcpClient(QObject* parent) : QTcpSocket(parent) {
+#include "util/util.h"
+
+const char* TcpClient::TEST_MESSAGE = "Test";
+
+TcpClient::TcpClient(QObject* parent) : QTcpSocket(parent) { /*, reconnectTimer(new QTimer(this))*/
+}
+
+TcpClient::~TcpClient() {
+    delete cryptographer;
 }
 
 qint64 TcpClient::encryptAndSend(const u_char* data, size_t length) {
-    std::string encyptedData = cryptographer.encryptData(data, length);
-    return QTcpSocket::write(encyptedData.c_str(), encyptedData.length());
+    std::string encryptedData = cryptographer->encryptData(data, length);
+
+    return QTcpSocket::write(encryptedData.c_str(), encryptedData.length());
 }
 
 QByteArray TcpClient::decryptData(const u_char* data, size_t length) {
-    std::string decryptedData = cryptographer.decryptData(data, length);
+    std::string decryptedData = cryptographer->decryptData(data, length);
     return QByteArray(decryptedData.c_str(), decryptedData.length());
 }
 
 const unsigned char* TcpClient::getPackedPublicKey() {
-    return cryptographer.getPublicKey();
+    return cryptographer->getPublicKey();
 }
 
 void TcpClient::calculateSecretKey(const unsigned char* key) {
-    cryptographer.deriveSecretKey(key);
+    cryptographer->deriveSecretKey(key);
 }
 
 const unsigned char* TcpClient::getSecretKey() {
-    return cryptographer.getSecretKey();
+    return cryptographer->getSecretKey();
 }
+
+void TcpClient::generateKeys() {
+    delete cryptographer;
+    cryptographer = new EcCrypto;
+}
+
+/*void TcpClient::switchConnectionToEncryptedMode() {
+    encryptedConn = true;
+}
+
+bool TcpClient::isConnectionEncrypted() const {
+    return encryptedConn;
+}*/
--- file-sharing-client/TcpClient/tcpclient.h
+++ file-sharing-client/TcpClient/tcpclient.h
@@ -2,25 +2,38 @@
 #define TCPCLIENT_H
 
 #include <QTcpSocket>
+#include <QTimer>
 
 #include "eccryptopp/eccryptopp.h"
 
 class TcpClient : public QTcpSocket {
     Q_OBJECT
 public:
+
+    static const char* TEST_MESSAGE;
+    static const int TEST_MESSAGE_LENGTH = 4;
+    static const int EXPECTED_TEST_MSG_LEN = 20;
+
     TcpClient(QObject* parent = nullptr);
+    virtual ~TcpClient();
 
     const unsigned char* getPackedPublicKey();
     void calculateSecretKey(const u_char* key);
+    void generateKeys();
     qint64 encryptAndSend(const u_char* data, size_t length);
     QByteArray decryptData(const u_char* data, size_t length);
+
+//    void switchConnectionToEncryptedMode();
+//    bool isConnectionEncrypted() const;
 
     // test function
     const unsigned char* getSecretKey();
 
 private:
+//    QTimer* reconnectTimer = nullptr;
 
-    EcCrypto cryptographer;
+//    bool encryptedConn = false;
+    EcCrypto* cryptographer = nullptr;
 };
 
 #endif // TCPCLIENT_H
