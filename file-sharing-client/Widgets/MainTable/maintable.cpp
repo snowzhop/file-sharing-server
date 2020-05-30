98385734c1d0a24d610415b7b21e0f4ee100e992
Поликарп <snowzhop@gmail.com>
2020-05-30 22:59:11 +0300

    Bugfix in the dropEvent function that broke the program when item was released on second or third cells

--- file-sharing-client/Widgets/MainTable/maintable.cpp
+++ file-sharing-client/Widgets/MainTable/maintable.cpp
@@ -7,9 +7,7 @@ MainTableWidget::MainTableWidget(QWidget* parent) : QTableWidget(parent) {}
 MainTableWidget::~MainTableWidget() {}
 
 void MainTableWidget::dropEvent(QDropEvent *event) {
-    if (event->source() != this) {
-        QTableWidget::dropEvent(event);
-    } else {
+    if (event->source() == this) {
         emit dropRowSignal(this->itemAt(event->pos())->row());
     }
 }
